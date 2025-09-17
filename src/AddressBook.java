350c09a3eebc4235db9d2b7d3c6c12f2633c30eb
akshavibaskaran <akshavi.bt@gmail.com>
2025-09-17 12:17:51 -0400

    Added a main method that prints "Adress Book"

---
+++ src/AddressBook.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo> buddyBook;
+
+    public AddressBook(){
+        buddyBook = new ArrayList<>();
+    }
+
+    public void addBuddy(BuddyInfo buddy){
+        if (buddy != null){
+            buddyBook.add(buddy);
+        }
+    }
+
+    public BuddyInfo removeBuddy(int index){
+        if (index >= 0 && index <= buddyBook.size()){
+            return buddyBook.remove(index);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("****** Address Book ******");
+    }
+}
+
